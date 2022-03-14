287bf9fa5e5d615bbce61715fe5550d654653730
Petar Petric <petricpetar22@gmail.com>
2022-03-14 02:46:07 +0100

    I couldnt sleep leaving JSON.stringify in there, so had to change it

--- src/store/modules/main-data-module/mutations.ts
+++ src/store/modules/main-data-module/mutations.ts
@@ -1,6 +1,6 @@
 import * as types from "./types";
 import state, { mainStateData } from "./state";
-import { ITableRow } from "./types";
+import { ITableRow, TREE_LEVEL } from "./types";
 
 const mutations = {
     [types.GET_DATA](state: mainStateData, data: ITableRow[]) {
@@ -8,20 +8,21 @@ const mutations = {
     },
 
     [types.REMOVE_NODE](state: mainStateData, data: ITableRow) {
-        recursiveRemoval(state.mainData, data)
+        recursiveRemoval(state.mainData, data, TREE_LEVEL.LEVEL_0)
     }
 };
 
-function recursiveRemoval(arr: any, obj: ITableRow) {
+function recursiveRemoval(arr: any, obj: ITableRow, comparator: string) {
     for (let i = 0; i < arr.length; i++) {
-        if (JSON.stringify(obj.data) == JSON.stringify(arr[i].data)) {
+        if (obj.data[comparator] == arr[i].data[comparator]) {
+            console.log(arr[i]);
             return arr.splice(i, 1)
         }
         if (arr[i].kids.has_relatives?.records) {
-            recursiveRemoval(arr[i].kids.has_relatives.records, obj);
+            recursiveRemoval(arr[i].kids.has_relatives.records, obj, TREE_LEVEL.LEVEL_1);
         }
         if (arr[i].kids.has_phone?.records) {
-            recursiveRemoval(arr[i].kids.has_phone.records, obj)
+            recursiveRemoval(arr[i].kids.has_phone.records, obj, TREE_LEVEL.LEVEL_2)
         }
     }
 }
--- src/store/modules/main-data-module/types.ts
+++ src/store/modules/main-data-module/types.ts
@@ -5,3 +5,8 @@ export interface ITableRow {
 
 export const GET_DATA = "GET_DATA";
 export const REMOVE_NODE = "REMOVE_NODE"
+export const TREE_LEVEL = {
+    LEVEL_0: "Identification number",
+    LEVEL_1: "Relative ID",
+    LEVEL_2: "Phone ID"
+}
